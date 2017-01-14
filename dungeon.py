0372e4cae06c3511cec40dcec40e7598d3d93f51
Weronika Sikorska <wmsikorska@gmail.com>
2017-01-15 00:44:41 +0100

    Added game logic - it's possible to play now

--- dungeon.py
+++ dungeon.py
@@ -12,7 +12,37 @@ class DungeonGameUrls(object):
         return self.base_url + "?attack=1"
 
     def get_move_url(self, direction):
+        print "going " + direction
         return self.base_url + "?m=" + direction
+
+    def get_treasure_url(self):
+        return self.base_url + "?tres=1"
+
+    # not tested
+    def send_get_treasure_request(self):
+        response = requests.get(self.get_treasure_url(), cookies=self.cookies)
+        return response.text
+
+    # not tested
+    def send_move_request(self, direction):
+        response = requests.get(self.get_move_url(direction), cookies=self.cookies)
+        return response.text
+
+    # not tested
+    def send_attack_request(self):
+        response = requests.get(self.get_attack_url(), cookies=self.cookies)
+        return response.text
+
+    # not tested
+    def send_base_request(self):
+        response = requests.get(self.base_url, cookies=self.cookies)
+        return response.text
+
+# TODO:
+# 1. Refactor tests (get.requests mock and url methods) to allow functional tests
+# 2. Refactor reading html - not so many requests
+# 3. Refactor moving logic?
+# 4. Usage of potions
 
 
 class DungeonGame(object):
@@ -20,22 +50,195 @@ class DungeonGame(object):
         self.stats = Stats(0, 0)
         self.board = Board()
         self.dungeon_game_urls = dungeon_game_urls
+        self.actions = Actions()
 
     def update_stats(self, url):
-        response = requests.get(url, cookies=self.dungeon_game_urls.cookies)
+        response = requests.get(url, cookies=self.dungeon_game_urls.cookies) #TODO: it should be from loaded html
         dungeon_parser = DungeonGameHtmlParser(response.text)
         self.stats = dungeon_parser.parse_game_stats()
 
+    # not tested
+    def get_actions(self, url):
+        response = requests.get(url, cookies=self.dungeon_game_urls.cookies) #TODO: it should be from loaded html
+        dungeon_parser = DungeonGameHtmlParser(response.text)
+        self.actions = dungeon_parser.parse_game_actions()
+
+    # not tested
+    def game_logic(self):
+        self.update_stats(self.dungeon_game_urls.base_url)
+        self.get_actions(self.dungeon_game_urls.base_url)
+        while self.actions is not None:
+            self.board = Board()
+            found_downstairs_and_ready = False
+            found_start_position = False
+            picked_up_treasure = None
+            while not found_downstairs_and_ready and self.actions is not None:
+                self.update_stats(self.dungeon_game_urls.base_url)
+                self.get_actions(self.dungeon_game_urls.base_url)
+
+                if self.actions.attack == 1:
+                    self.attack()
+                elif self.actions.treasure is not None and picked_up_treasure is None:
+                    picked_up_treasure = self.pick_up_treasure()
+                elif self.actions.downstairs == 1:
+                    picked_up_treasure = None
+                    print "downstairs?"
+                    print "action down: ", self.actions.downstairs
+                    print "stats: ", self.stats.level, self.stats.dungeon_level
+                    print "current pos ", self.board.current_position
+                    self.board.downstairs = self.board.current_position
+                    found_downstairs_and_ready = self.move_downstairs()
+                elif found_start_position:
+                    picked_up_treasure = None
+                    print "action down: ", self.actions.downstairs
+                    print "stats: ", self.stats.level, self.stats.dungeon_level
+                    print "current pos ", self.board.current_position
+                    self.move()  # this appends to board
+                else:
+                    print "looking for start pos"
+                    picked_up_treasure = None
+                    self.board = Board()
+                    found_start_position = self.move_to_start_position()
+
+    # not tested
     def attack(self):
-        response = requests.get(self.dungeon_game_urls.base_url, cookies=self.dungeon_game_urls.cookies)
-        if "Attack!" in response.content:
-            attack_response = requests.get(self.dungeon_game_urls.get_attack_url(), cookies=self.dungeon_game_urls.cookies)
-            print "Attacked", attack_response.text
-        # TODO: remember about new gear
+        return self.dungeon_game_urls.send_attack_request()
+
+    # not tested
+    def pick_up_treasure(self):
+        if "potion" not in self.actions.treasure.lower():
+            if self.stats.compare_weapons(
+                    DungeonGameHtmlParser.parse_weapon(self.actions.treasure), self.stats.weapon):
+                self.dungeon_game_urls.send_get_treasure_request()
+                return True
+        return False
+
+    # not tested
+    def move(self):
+        if self.board.downstairs is not None:
+            print "only wandering s-n our downstairs is ", self.board.downstairs, "our position ", self.board.current_position
+            if self.board.is_south(self.board.downstairs):
+                self.board.move_south()
+                self.dungeon_game_urls.send_move_request('s')
+            elif self.board.is_north(self.board.downstairs):
+                self.board.move_north()
+                self.dungeon_game_urls.send_move_request('n')
+            elif self.board.is_east(self.board.downstairs):
+                self.board.move_east()
+                self.dungeon_game_urls.send_move_request('e')
+            elif self.board.is_west(self.board.downstairs):
+                self.board.move_west()
+                self.dungeon_game_urls.send_move_request('w')
+            else:
+                print "we are on a downstairs position but we are not ready"
+                if self.actions.move_south == 1:
+                    self.board.move_south()
+                    self.dungeon_game_urls.send_move_request('s')
+                elif self.actions.move_north == 1:
+                    self.board.move_north()
+                    self.dungeon_game_urls.send_move_request('n')
+        else:
+            if self.actions.move_east == 1 and not self.board.been_east():
+                self.board.move_east()
+                self.dungeon_game_urls.send_move_request('e')
+            elif self.actions.move_west == 1 and not self.board.been_west():
+                self.board.move_west()
+                self.dungeon_game_urls.send_move_request('w')
+            elif self.actions.move_north == 1 and ((self.board.been_east() and self.actions.move_west != 1) ^
+                                                       (self.board.been_west() and self.actions.move_east != 1)):
+                self.board.move_north()
+                self.dungeon_game_urls.send_move_request('n')
+            else:
+                print "#### i think it's unused code - ~146"
+                self.board.move_south()
+                self.dungeon_game_urls.send_move_request('s')
+
+    # not tested
+    def move_downstairs(self):
+        print "deciding if moving downstairs ", self.stats.level, " ", self.stats.dungeon_level
+        if self.stats.level - 2 > self.stats.dungeon_level:
+            self.dungeon_game_urls.send_move_request("d")
+            self.board = Board()
+            return True
+        else:
+            self.move()
+            return False
+
+    # not tested
+    def move_to_start_position(self):
+        if self.actions.move_south == 1:
+            self.dungeon_game_urls.send_move_request("s")
+            return False
+        elif self.actions.move_west == 1:
+            self.dungeon_game_urls.send_move_request("w")
+            return False
+        return True
 
 
-class Board():
-    pass
+class Board(object):
+    def __init__(self):
+        self.board = {0: [0]}
+        self.current_position = (0, 0)
+        self.downstairs = None
+
+    def move_north(self):
+        x, y = self.current_position
+        self.current_position = (x, y+1)
+        self._append_position_to_board()
+
+    def move_east(self):
+        x, y = self.current_position
+        self.current_position = (x+1, y)
+        self._append_position_to_board()
+
+    def move_west(self):
+        x, y = self.current_position
+        self.current_position = (x-1, y)
+        self._append_position_to_board()
+
+    def been_west(self):
+        x, y = self.current_position
+        if self.board.get(x-1) is None:
+            return False
+        return y in self.board.get(x-1)
+
+    def been_east(self):
+        x, y = self.current_position
+        if self.board.get(x+1) is None:
+            return False
+        return y in self.board.get(x+1)
+
+    def move_south(self):
+        x, y = self.current_position
+        self.current_position = (x, y-1)
+        self._append_position_to_board()
+
+    def _append_position_to_board(self):
+        x, y = self.current_position
+        if self.board.get(x) is None:
+            self.board[x] = [y]
+        elif y not in self.board.get(x):
+            self.board[x].append(y)
+
+    def is_south(self, position):
+        _x_current, y_current = self.current_position
+        _x_pos, y_pos = position
+        return y_pos < y_current
+
+    def is_north(self, position):
+        _x_current, y_current = self.current_position
+        x_pos, y_pos = position
+        return y_pos > y_current
+
+    def is_east(self, position):
+        x_current, _y_current = self.current_position
+        x_pos, _y_pos = position
+        return x_pos > x_current
+
+    def is_west(self, position):
+        x_current, _y_current = self.current_position
+        x_pos, _y_pos = position
+        return x_pos < x_current
 
 
 class DungeonGameHtmlParser(object):
@@ -62,7 +265,17 @@ class DungeonGameHtmlParser(object):
         return stats
 
     def parse_game_actions(self):
-        pass
+        if 'stands before you' in self.game_html:
+            print self.game_html
+            return None
+        soup_href = self.parser.find_all('a')
+        actions_dict = {}
+        for tag in soup_href:
+            if '?tres' in tag.get('href'):
+                actions_dict['treasure'] = tag.text
+            else:
+                actions_dict[tag.text.lower().replace('!', '').replace(' ', '')] = 1
+        return Actions(**actions_dict)
 
     @staticmethod
     def parse_weapon(weapon_string):
@@ -85,6 +298,17 @@ class Actions(object):
         self.move_west = kwargs.get('west')
         self.move_east = kwargs.get('east')
         self.attack = kwargs.get('attack')
+        self.treasure = kwargs.get('treasure')
+        self.downstairs = kwargs.get('downstairs')
+
+    def __str__(self):
+        return str({"n": self.move_north,
+                    "s": self.move_north,
+                    "w": self.move_west,
+                    "e": self.move_east,
+                    "attack": self.attack,
+                    "tres": self.treasure,
+                    "d": self.downstairs})
 
 
 class Stats(object):
@@ -103,3 +327,10 @@ class Stats(object):
             return level1 > level2
         else:
             return addition1 > addition2
+
+    def __str__(self):
+        return str({"level": self.level,
+                    "health": self.health,
+                    "inv": self.inventory,
+                    "weapon": self.weapon,
+                    "dungeon_level": self.dungeon_level})
--- test_dungeon.py
+++ test_dungeon.py
@@ -15,13 +15,23 @@ class TestDungeonGame(unittest.TestCase):
     def get_test_url():
         return "http://dungeon-game.com/index.php"
 
+    @staticmethod
+    def get_test_cookies():
+        return {"random-cookie": 'value'}
+
     def mocked_requests_get(*args, **kwargs):
-        dungeon_urls = dungeon.DungeonGameUrls(TestDungeonGame.get_test_url(), {})
+        dungeon_urls = dungeon.DungeonGameUrls(TestDungeonGame.get_test_url(), TestDungeonGame.get_test_cookies())
         if args[0] == dungeon_urls.base_url:
+            with open("content/base.html") as f:
+                return MockResponse(f.read(), 200)
+        if args[0] == dungeon_urls.get_treasure_url():
             with open("content/treasure.html") as f:
                 return MockResponse(f.read(), 200)
         elif args[0] == dungeon_urls.get_attack_url():
             with open("content/attack.html") as f:
+                return MockResponse(f.read(), 200)
+        elif args[0] == dungeon_urls.get_move_url('d'):
+            with open("content/downstairs.html") as f:
                 return MockResponse(f.read(), 200)
         # TODO: add more requests
         return MockResponse({}, 404)
@@ -30,11 +40,46 @@ class TestDungeonGame(unittest.TestCase):
     def test_update_stats_from_base_url(self, get_mock):
         self.dungeon.update_stats(self.dungeon_urls.base_url)
         get_mock.assert_called_once_with(self.dungeon_urls.base_url, cookies=self.cookies)
+        self.assertIsNotNone(self.dungeon.stats)
 
     @patch('requests.get', side_effect=mocked_requests_get)
-    def test_update_stats_from_base_url(self, get_mock):
+    def test_update_stats_from_attack_url(self, get_mock):
         self.dungeon.update_stats(self.dungeon_urls.get_attack_url())
         get_mock.assert_called_once_with(self.dungeon_urls.get_attack_url(), cookies=self.cookies)
+        self.assertIsNotNone(self.dungeon.stats)
+
+    @patch('requests.get', side_effect=mocked_requests_get)
+    def test_update_actions_from_base_url(self, get_mock):
+        self.dungeon.get_actions(self.dungeon_urls.base_url)
+        get_mock.assert_called_once_with(self.dungeon_urls.base_url, cookies=self.cookies)
+        self.assertIsNotNone(self.dungeon.actions)
+
+    @patch('requests.get', side_effect=mocked_requests_get)
+    def test_update_actions_from_attack_url(self, get_mock):
+        self.dungeon.get_actions(self.dungeon_urls.get_attack_url())
+        get_mock.assert_called_once_with(self.dungeon_urls.get_attack_url(), cookies=self.cookies)
+        self.assertIsNotNone(self.dungeon.actions)
+
+    @patch('requests.get', side_effect=mocked_requests_get)
+    def test_pick_up_treasure_from_treasure_url(self, get_mock):
+        self.dungeon.update_stats(self.dungeon_urls.get_treasure_url())
+        self.dungeon.get_actions(self.dungeon_urls.get_treasure_url())
+        self.dungeon.pick_up_treasure()
+        get_mock.assert_called(self.dungeon_urls.send_get_treasure_request())
+
+    @patch('requests.get', side_effect=mocked_requests_get)
+    def test_move_downstairs_from_downstairs_url_when_level_enough(self, get_mock):
+        self.dungeon.update_stats(self.dungeon_urls.get_move_url('d'))
+        self.dungeon.get_actions(self.dungeon_urls.get_move_url('d'))
+        self.assertTrue(self.dungeon.move_downstairs())
+        get_mock.assert_called()
+
+    @patch('requests.get', side_effect=mocked_requests_get)
+    def test_move_downstairs_from_downstairs_url_when_level_not_enough(self, get_mock):
+        self.dungeon.update_stats(self.dungeon_urls.get_move_url('d'))
+        self.dungeon.get_actions(self.dungeon_urls.get_move_url('d'))
+        self.dungeon.stats.level = self.dungeon.stats.dungeon_level - 1
+        self.assertFalse(self.dungeon.move_downstairs())
 
 
 class TestDungeonGameUrls(unittest.TestCase):
@@ -52,6 +97,14 @@ class TestDungeonGameUrls(unittest.TestCase):
     def test_dungeon_urls_move_south_url(self):
         dungeon_urls = dungeon.DungeonGameUrls(self.test_base_url, {})
         self.assertEqual(self.test_base_url + "?m=s", dungeon_urls.get_move_url("s"))
+
+    def test_dungeon_urls_move_downstairs_url(self):
+        dungeon_urls = dungeon.DungeonGameUrls(self.test_base_url, {})
+        self.assertEqual(self.test_base_url + "?m=d", dungeon_urls.get_move_url("d"))
+
+    def test_dungeon_urls_treasure_url(self):
+        dungeon_urls = dungeon.DungeonGameUrls(self.test_base_url, {})
+        self.assertEqual(self.test_base_url + "?tres=1", dungeon_urls.get_treasure_url())
 
 
 class TestDungeonGameHtmlParser(unittest.TestCase):
@@ -71,11 +124,7 @@ class TestDungeonGameHtmlParser(unittest.TestCase):
         with open("content/treasure.html") as f:
             dungeon_parser = dungeon.DungeonGameHtmlParser(f.read())
             stats = dungeon_parser.parse_game_stats()
-            self.assertEqual(expected_stats.health, stats.health)
-            self.assertEqual(expected_stats.level, stats.level)
-            self.assertEqual(expected_stats.inventory, stats.inventory)
-            self.assertEqual(expected_stats.weapon, stats.weapon)
-            self.assertEqual(expected_stats.dungeon_level, stats.dungeon_level)
+            self._assert_equal_stats(expected_stats, stats)
 
     def test_parse_game_html_attack(self):
         expected_stats = dungeon.Stats(33, 603)
@@ -85,18 +134,60 @@ class TestDungeonGameHtmlParser(unittest.TestCase):
         with open("content/attack.html") as f:
             dungeon_parser = dungeon.DungeonGameHtmlParser(f.read())
             stats = dungeon_parser.parse_game_stats()
-            self.assertEqual(expected_stats.health, stats.health)
-            self.assertEqual(expected_stats.level, stats.level)
-            self.assertEqual(expected_stats.inventory, stats.inventory)
-            self.assertEqual(expected_stats.weapon, stats.weapon)
-            self.assertEqual(expected_stats.dungeon_level, stats.dungeon_level)
+            self._assert_equal_stats(expected_stats, stats)
 
-    def test_parse_html_possible_actions_base_url(self):
-        expected_actions = dungeon.Actions(south=1, north=1)
+    def test_parse_game_html_downstairs(self):
+        expected_stats = dungeon.Stats(44, 100)
+        expected_stats.inventory = [u'A potion +5', u'B potion +4', u'C potion +4']
+        expected_stats.weapon = ("Tester", 30, 1)
+        expected_stats.dungeon_level = 31
+        with open("content/downstairs.html") as f:
+            dungeon_parser = dungeon.DungeonGameHtmlParser(f.read())
+            stats = dungeon_parser.parse_game_stats()
+            self._assert_equal_stats(expected_stats, stats)
+
+    def test_parse_html_possible_actions_attack_url_without_boss(self):
+        expected_actions = dungeon.Actions(attack=1)
+        with open("content/attack.html") as f:
+            dungeon_parser = dungeon.DungeonGameHtmlParser(f.read())
+            actions = dungeon_parser.parse_game_actions()
+            self._assert_equal_actions(expected_actions, actions)
+
+    def test_parse_html_possible_actions_attack_url_with_boss(self):
+        with open("content/boss.html") as f:
+            dungeon_parser = dungeon.DungeonGameHtmlParser(f.read())
+            actions = dungeon_parser.parse_game_actions()
+            self.assertIsNone(actions)
+
+    def test_parse_html_possible_actions_treasure_url(self):
+        expected_actions = dungeon.Actions(south=1, north=1, east=1, treasure=u'Level 31 Tester +2')
         with open("content/treasure.html") as f:
             dungeon_parser = dungeon.DungeonGameHtmlParser(f.read())
             actions = dungeon_parser.parse_game_actions()
-            self.assertEqual(expected_actions.move_north, actions.move_north)
+            self._assert_equal_actions(expected_actions, actions)
+
+    def test_parse_html_possible_actions_downstairs_url(self):
+        expected_actions = dungeon.Actions(west=1, south=1, east=1, downstairs=1)
+        with open("content/downstairs.html") as f:
+            dungeon_parser = dungeon.DungeonGameHtmlParser(f.read())
+            actions = dungeon_parser.parse_game_actions()
+            self._assert_equal_actions(expected_actions, actions)
+
+    def _assert_equal_actions(self, expected_actions, actions):
+        self.assertEqual(expected_actions.move_north, actions.move_north)
+        self.assertEqual(expected_actions.move_south, actions.move_south)
+        self.assertEqual(expected_actions.move_west, actions.move_west)
+        self.assertEqual(expected_actions.move_east, actions.move_east)
+        self.assertEqual(expected_actions.attack, actions.attack)
+        self.assertEqual(expected_actions.treasure, actions.treasure)
+        self.assertEqual(expected_actions.downstairs, actions.downstairs)
+
+    def _assert_equal_stats(self, expected_stats, stats):
+        self.assertEqual(expected_stats.health, stats.health)
+        self.assertEqual(expected_stats.level, stats.level)
+        self.assertEqual(expected_stats.inventory, stats.inventory)
+        self.assertEqual(expected_stats.weapon, stats.weapon)
+        self.assertEqual(expected_stats.dungeon_level, stats.dungeon_level)
 
 
 class TestStats(unittest.TestCase):
@@ -117,6 +208,75 @@ class TestStats(unittest.TestCase):
         weapon2 = ("Tester", 29, 1)
         self.assertEqual(True, dungeon.Stats.compare_weapons(weapon1, weapon2))
         self.assertEqual(False, dungeon.Stats.compare_weapons(weapon2, weapon1))
+
+
+class TestBoard(unittest.TestCase):
+    def setUp(self):
+        self.board = dungeon.Board()
+
+    def test_move_north_from_scratch(self):
+        self.board.move_north()
+        self.assertEqual((0, 1), self.board.current_position)
+        self.assertEqual({0: [0, 1]}, self.board.board)
+
+    def test_move_north_to_existing(self):
+        expected_board = {0: [0, 1], 1: [0, 1]}
+        self.board.board = expected_board
+        self.board.current_position = (1, 0)
+        self.board.move_north()
+        self.assertEqual((1, 1), self.board.current_position)
+        self.assertEqual(expected_board, self.board.board)
+
+    def test_move_south_from_scratch(self):
+        self.board.move_south()
+        self.assertEqual((0, -1), self.board.current_position)
+        self.assertEqual({0: [0, -1]}, self.board.board)
+
+    def test_move_south_to_existing(self):
+        expected_board = {0: [0, 1], 1: [0, 1]}
+        self.board.board = expected_board
+        self.board.current_position = (1, 1)
+        self.board.move_south()
+        self.assertEqual((1, 0), self.board.current_position)
+        self.assertEqual(expected_board, self.board.board)
+
+    def test_move_east_from_scratch(self):
+        self.board.move_east()
+        self.assertEqual((1, 0), self.board.current_position)
+        self.assertEqual({0: [0], 1: [0]}, self.board.board)
+
+    def test_move_east_to_existing(self):
+        expected_board = {0: [0, 1], 1: [0, 1]}
+        self.board.board = expected_board
+        self.board.current_position = (0, 1)
+        self.board.move_east()
+        self.assertEqual((1, 1), self.board.current_position)
+        self.assertEqual(expected_board, self.board.board)
+
+    def test_move_west_from_scratch(self):
+        self.board.move_west()
+        self.assertEqual((-1, 0), self.board.current_position)
+        self.assertEqual({0: [0], -1: [0]}, self.board.board)
+
+    def test_move_west_to_existing(self):
+        expected_board = {0: [0, 1], -1: [0, 1]}
+        self.board.board = expected_board
+        self.board.current_position = (0, 1)
+        self.board.move_west()
+        self.assertEqual((-1, 1), self.board.current_position)
+        self.assertEqual(expected_board, self.board.board)
+
+    def test_is_south(self):
+        self.assertTrue(self.board.is_south((0, -1)))
+
+    def test_is_north(self):
+        self.assertTrue(self.board.is_north((0, 1)))
+
+    def test_is_east(self):
+        self.assertTrue(self.board.is_east((1, 0)))
+
+    def test_is_west(self):
+        self.assertTrue(self.board.is_west((-1, 0)))
 
 
 class MockResponse(object):
